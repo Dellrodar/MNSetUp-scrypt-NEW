ba28c2708b099c3e976b946aaa53290a57ca94b6
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2018-06-20 10:31:42 -0400

    Add files via upload
    added update script. Big thanks to Drethic.

---
+++ trtt-update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root." 1>&2
+   exit 1
+fi
+
+pushd /root
+killall trittiumd
+rm /usr/local/bin/trittium*
+wget https://github.com/Trittium/trittium/releases/download/2.1.0/Trittium-2.1.0-Ubuntu-daemon.tgz
+tar -xvf Trittium-2.1.0-Ubuntu-daemon.tgz -C /usr/local/bin/
+rm Trittium-2.1.0-Ubuntu-daemon.tgz
+apt-get -qq install aptitude
+
+aptitude -y -q install fail2ban
+service fail2ban restart
+
+apt-get -qq install ufw
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw allow 30001/tcp
+yes | ufw enable
+
+su tritt
+trittiumd -daemon
+
+echo "To verify you blockchain is sync'd run:"
+echo "watch trittium-cli getblockcount"
